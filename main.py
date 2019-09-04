3603150a0823b81f2e09572c1c966fa709602cc1
DanielAlonsoTM <danielinogordo@gmail.com>
2019-09-04 04:30:44 -0400

    Check if command exists.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ os.system(cmd_ls)
 cmd_nmap = 'nmap -sP 192.168.1.1/24'
 status, result = subprocess.getstatusoutput(cmd_nmap)
 
-if status == 0:
+if status:
     os.system(cmd_nmap)
 else:
     print("nmap is not installed")
