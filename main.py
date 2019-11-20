0be832b0ab93cd21bd04b87f112d804410dae5af
DanielAlonsoTM <danielinogordo@gmail.com>
2019-11-20 09:07:09 -0300

    Remake function establish_connection

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from module_packages.utilities import check_package_installed, install_package
 from module_network.scanning import check_hosts_network
+from module_network.wifi_connecting import establish_connection
+from utils.read_ini import read_config_ini
+import os
 
-cmd = "nmap"
-if check_package_installed(cmd):
-    print("Package is installed")
-else:
-    print("Package is not installed")
-    # install_package(cmd)
+# cmd = "nmap"
+# if check_package_installed(cmd):
+#     print("Package is installed")
+# else:
+#     print("Package is not installed")
+#     install_package(cmd)
 
-check_hosts_network()
+# check_hosts_network()
+
+establish_connection("RedRun_104", "!Stalk3r_greeD1*08")
--- module_network/wifi_connecting.py
+++ module_network/wifi_connecting.py
@@ -1,14 +1,23 @@
 import os
+from utils.read_ini import read_config_ini
 
 
 def establish_connection(ssdi, password):
     try:
-        # Edit Wifi name and password for config file
-        cmd = "wpa_passphrase " + ssdi + " " + password + " > /etc/wpa_supplicant.conf"
-        os.system(cmd)
+        cmd_wpa_passphrase = 'wpa_passphrase "{}" "{}"  >> /etc/wpa_supplicant/wpa_supplicant.conf'.format(ssdi, password)
+
+        # Get user credential
+        key = read_config_ini('DEFAULT', 'password')
+
+        cmd_wpa_passphrase = "echo {} | sudo -S sh -c '{}'".format(key, cmd_wpa_passphrase)
+
+        # Add new SSID to /etc/wpa_supplicant/wpa_supplicant.conf
+        os.system(cmd_wpa_passphrase)
 
         # Make connection
-        cmd = "wpa_supplicant -B -i interface -c /etc/wpa_supplicant.conf"
-        os.system(cmd)
+        # cmd_wpa_supplicant = "wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf"
+        cmd_wpa_supplicant = "echo {} | sudo -S wpa_cli -i wlan0 reconfigure".format(key)
+        os.system(cmd_wpa_supplicant)
+
     except OSError as error:
         print(error)
